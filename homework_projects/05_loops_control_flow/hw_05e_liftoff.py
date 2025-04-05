4c429d1d764b4702a055178273179db516646db1
samiyamarium <160397803+samiyamarium@users.noreply.github.com>
2025-04-06 02:39:49 +0500

    Add files via upload

---
+++ homework_projects/05_loops_control_flow/hw_05e_liftoff.py
@@ -0,0 +1,19 @@
+def main():
+    print("Spaceship liftoff ! :)")
+    
+
+
+for i in range(10):
+    t=10
+    if i<=10:
+        t=t-i
+        print(t)
+    else:
+         print("countdown continues!!")
+
+        
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
---
+++ homework_projects/05_loops_control_flow/hw_05f_double_it.py
@@ -0,0 +1,13 @@
+print("Double the number :)")
+def main():
+  print("")
+x=float(input("Enter your number : "))
+while x<100:
+   x=x*2
+   print(x)
+
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
